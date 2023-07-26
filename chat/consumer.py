6fcac889d14c6752a7360dc9b77271dcd29eb393
Jiyesss <choijiyea7185@gmail.com>
2023-07-26 21:46:00 +0900

    07/02

--- chat/consumer.py
+++
@@ -1,8 +0,0 @@
-from channels.generic.websocket import JsonWebsocketConsumer
-
-
-class RolePlayingRoomConsumer(JsonWebsocketConsumer):
-    def receive_json(self, content, **kwargs):
-        print("received:", content)
-        # Echo
-        self.send_json(content)
