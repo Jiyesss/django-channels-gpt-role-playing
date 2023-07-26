b4072cd80d1eae9023806080556e79addbc7c9ab
Jiyesss <choijiyea7185@gmail.com>
2023-07-26 18:58:03 +0900

    05/04

--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,21 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView, ListView
 from .models import RolePlayingRoom
 from .forms import RolePlayingRoomForm
+
+@method_decorator(staff_member_required, name="dispatch")
+class RolePlayingRoomListView(ListView):
+    model = RolePlayingRoom
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.filter(user=self.request.user)
+        return qs
+
+
+role_playing_room_list = RolePlayingRoomListView.as_view()
 
 @method_decorator(staff_member_required, name="dispatch")
 class RolePlayingRoomCreateView(CreateView):
@@ -16,3 +28,16 @@ class RolePlayingRoomCreateView(CreateView):
         return super().form_valid(form)
 
 role_playing_room_new = RolePlayingRoomCreateView.as_view()
+
+@method_decorator(staff_member_required, name="dispatch")
+class RolePlayingRoomUpdateView(UpdateView):
+    model = RolePlayingRoom
+    form_class = RolePlayingRoomForm
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.filter(user=self.request.user)
+        return qs
+
+
+role_playing_room_edit = RolePlayingRoomUpdateView.as_view()
